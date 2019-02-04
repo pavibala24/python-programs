4484be8d4cfcd3887325c1679315164f16ee5a28
pavibala24 <37238733+pavibala24@users.noreply.github.com>
2019-02-04 12:50:06 +0530

    Create leap year checking.py

---
+++ leap year checking.py
@@ -0,0 +1,11 @@
+year = 2016
+if (year % 4) == 0:
+ if (year % 100) == 0:
+   if (year % 400) == 0:
+     print("{0} is a LEAP year".format(year))
+   else:
+     print("{0} is not a LEAP year".format(year))
+ else:
+   print("{0} is a LEAP year".format(year))
+else:
+  print("{0} is not a LEAP year".format(year))
