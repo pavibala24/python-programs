b8c769afdaf88261962ec275c67b1140c50ba543
pavibala24 <37238733+pavibala24@users.noreply.github.com>
2019-02-05 10:46:41 +0530

    Create alphabet or not.py

---
+++ alphabet or not.py
@@ -0,0 +1,8 @@
+print("Check whether the character is alphabet or not")
+print("----------------------------------------------")
+p = input("Enter the character: ")
+if((p>='a' and p<= 'z') or (p>='A' and p<='Z')):
+    print(p, " it is an Alphabet")
+else:
+    print(p, " No, it is not an Alphabet")
+  
