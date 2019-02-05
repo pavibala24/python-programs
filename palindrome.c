6548fa1c35edb7e7db072d90b64f4f61ea09e1e4
pavibala24 <37238733+pavibala24@users.noreply.github.com>
2019-02-05 15:29:11 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n,reverse=0,temp;
+{
+printf("n=");
+scanf("%d",&n);
+{
+temp=n;
+while(temp!=0)
+{
+reverse=reverse*10;
+reverse=reverse+temp%10;
+temp=temp/10;
+}
+if(temp=reverse)
+printf("%d is a palindrome",n);
+else
+printf("%d is not an palindrome",n);
+}
+}
+}
