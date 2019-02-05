ac6351a004eef86bc30237b0b6fb834a79c08c95
pavibala24 <37238733+pavibala24@users.noreply.github.com>
2019-02-05 10:22:59 +0530

    Update vowel or consonant.py

--- vowel or consonant.py
+++ vowel or consonant.py
@@ -1,4 +1,4 @@
-print("To check the letter is vwel or consonant")
+print("To check the letter is vowel or consonant")
 a = input("Enter character: ")
 if(a =='A' or a =='a' or a =='E' or a =='e' or a =='I'
  or a=='i' or a =='O' or a =='o' or a =='U' or a =='u'):
