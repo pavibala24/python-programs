607a2a6dc1fc76ec3e9bee01afabee07b6d29c0c
pavibala24 <37238733+pavibala24@users.noreply.github.com>
2019-02-05 11:42:08 +0530

    Update integer.c

--- integer.c
+++ integer.c
@@ -3,11 +3,11 @@ void main()
 {
 int a[]={0,1,2,3,4,5,6,7,8,9};
 int sum=0,n,k,i;
- printf("enter n value");
+ printf("enter value of N");
   scanf("%d",&n);
   for(i=0;i<n;i++)
     printf("%d",a[i]);
-  printf("enter K value");
+  printf("enter value of K");
   scanf("%d",&k);
   for(i=0;i<=k;K++)
   {
