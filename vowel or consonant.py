8c4bbcebea59fbcf160c4dad6c5645bdd3171b56
pavibala24 <37238733+pavibala24@users.noreply.github.com>
2019-02-05 10:18:41 +0530

    Create vowel or consonant.py

---
+++ vowel or consonant.py
@@ -0,0 +1,7 @@
+print("To check the letter is vwel or consonant")
+a = input("Enter character: ")
+if(a =='A' or a =='a' or a =='E' or a =='e' or a =='I'
+ or a=='i' or a =='O' or a =='o' or a =='U' or a =='u'):
+    print(a, "is a VOWEL")
+else:
+    print(a, "is a CONSONANT")
